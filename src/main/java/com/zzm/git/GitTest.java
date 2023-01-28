1079caf03dbd7e4870aaa888c4feca2fa3cf2dbb
zzm <1668919776@qq.com>
2023-01-29 00:52:25 +0800

    hot-fix commit1

--- src/main/java/com/zzm/git/GitTest.java
+++ src/main/java/com/zzm/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git");
 
     }
 }
