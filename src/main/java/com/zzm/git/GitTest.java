3f44585def6f3a1b0afc7be2e89c869e12d48d3c
zzm <1668919776@qq.com>
2023-01-29 00:39:54 +0800

    first commit

---
+++ src/main/java/com/zzm/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.zzm.git;
+
+/**
+ * @author ZZM
+ * @content
+ * @creat 2023-01-29-0:35
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
