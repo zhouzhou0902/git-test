26c1fb3d6ff09d3e5fb48f351f09144a586142cd
zzm <1668919776@qq.com>
2023-01-29 23:24:48 +0800

    gitee commit

--- src/main/java/com/zzm/git/GitTest.java
+++ src/main/java/com/zzm/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("hello git5");
         System.out.println("hello git6");
+        System.out.println("hello git7");
     }
 }
