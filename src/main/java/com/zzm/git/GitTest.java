32ffc4655e23bb39d93491708f8917b3c14bda88
zhouzhou0902 <123760906+zhouzhou0902@users.noreply.github.com>
2023-01-29 17:43:43 +0800

    11

--- src/main/java/com/zzm/git/GitTest.java
+++ src/main/java/com/zzm/git/GitTest.java
@@ -7,11 +7,12 @@ package com.zzm.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("hello git5");
+        System.out.println("hello git6");
 
     }
 }
