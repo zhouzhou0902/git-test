a672864706de4c10990ae226b399f930c438157a
zzm <1668919776@qq.com>
2023-01-29 17:42:52 +0800

    first commit

--- src/main/java/com/zzm/git/GitTest.java
+++ src/main/java/com/zzm/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git");
 
     }
 }
